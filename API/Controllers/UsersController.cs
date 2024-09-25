9e15878c33039b793b3e8bddae3b2746752d3a3f
Paul Dequidt <pdequidt@hotmail.com>
2024-09-25 21:49:38 +0200

    End of Section 2

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,31 +1,31 @@
+using System;
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[Authorize]
-public class UsersController(DataContext context) : BaseApiController
+[ApiController]
+[Route("api/[controller]")] // api/users
+public class UsersController(DataContext context) : ControllerBase
 {
-    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         var users = await context.Users.ToListAsync();
-
+        
         return users;
     }
-        [AllowAnonymous]
-        [HttpGet("{id:int}")] // /api/users/2
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        {
-            var user = await context.Users.FindAsync(id);
 
-            if (user == null) return NotFound();
-                
-            return user;
-        }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        
+        if (user == null) return NotFound();
+        
+        return user;
+    }
+    
 }
-   
--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : BaseApiController
+public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
